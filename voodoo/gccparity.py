9cef4d41ab84a2e78e3a05826f15b1852e4f06a6
Sébastien Morin <smsisko@gmail.com>
2015-05-12 10:46:09 -0400

    Update to the result of gccIncludePath, we only add dir (we had a false positivie on some executable) and we present the list with / seperator on every platform.

--- voodoo/gccparity.py
+++ voodoo/gccparity.py
@@ -11,5 +11,8 @@ def gccIncludePath():
     if _cachedGCCIncludePath is None:
         with open( os.devnull, "r" ) as noInput:
             output = subprocess.check_output( [ "g++", "-E", "-x", "c++", "-", "-v" ], stderr = subprocess.STDOUT, stdin = noInput )
-            _cachedGCCIncludePath = re.findall( r"\r?\n (\S+)", output )
+            _cachedGCCIncludePath = list()
+            for filename in re.findall( r"\r?\n (\S+)", output ):
+                if os.path.isdir(filename):
+                    _cachedGCCIncludePath.append(os.path.normpath(filename).replace("\\","/"))
     return _cachedGCCIncludePath
