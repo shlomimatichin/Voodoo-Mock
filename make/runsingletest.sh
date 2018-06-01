c120627945ddc37e7c760013f785ce052d025696
Shlomi Matichin <shlomomatichin@gmail.com>
2018-06-01 22:05:43 +0300

    refactoring

--- make/runsingletest.sh
+++ make/runsingletest.sh
@@ -10,14 +10,14 @@ function isCxxTest {
 }
 
 if isPython $SUITE; then
-    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/gettest.py $SUITE $REGEX_OR_LINE_NUMBER`
+    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/findtestname.py $SUITE $REGEX_OR_LINE_NUMBER`
 	python $VOODOO_ROOT_DIR/pytest/pytestrunner.py --verbose --singleTest=$TEST_NAME $SUITE
 else
     if [ ! isCxxTest ]; then
         echo "$SUITE was not recognized as niether python test suite or CxxTest suite files"
         exit 1
     fi
-    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/gettest.py $SUITE $REGEX_OR_LINE_NUMBER`
+    TEST_NAME=`python $VOODOO_ROOT_DIR/pytest/findtestname.py $SUITE $REGEX_OR_LINE_NUMBER`
     NO_EXT=`echo $SUITE | sed 's@\.[hH].\?.\?$@@'`
     NO_EXT_UNDERSCORE=`echo $NO_EXT | sed 's@/@_@g'`
     BIN=build_unittest/${NO_EXT_UNDERSCORE}.bin
